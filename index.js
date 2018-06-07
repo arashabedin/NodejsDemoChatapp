de915d28ad5d053c62757e28b11a5accb207a9fa
arashabedin <arash.abedin@gmail.com>
2018-06-07 15:11:13 +0200

    initial prototype

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req,res){
+res.sendFile(__dirname+"/index.html");
+});
+
+io.on('connection',function(socket){
+io.emit('stream connect');
+socket.on('chat happend', function(msg){
+//console.log(msg);
+io.emit('stream chat',msg);
+});
+socket.on('disconnect', function(){
+io.emit('stream disconnect');
+});
+});
+
+http.listen(3000, function(){
+console.log('listening on *:3000');
+});
